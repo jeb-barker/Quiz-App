a25081f5e553f4c1051a93f02c3495de8228a2fe
James Barker <jeb.e.barker@gmail.com>
2020-09-10 16:30:34 -0400

    added a second textBox
    If "TJ" is written when the first text box loses focus, TJ Rocks will be displayed, and the hint on the first text box will be set to "Thats a nice name"

--- app/src/main/java/com/barkerjeb/quizapp/MainActivity.java
+++ app/src/main/java/com/barkerjeb/quizapp/MainActivity.java
@@ -7,21 +7,44 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
+    EditText textBox;
+    TextView shownText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         submitButton=findViewById(R.id.clickButton);
+        textBox=findViewById(R.id.editText);
+        shownText=findViewById(R.id.textBox);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Hello Mom!");
-                Log.i("testButton", "Hi Dad!");
+                Log.i("testButton", "Hi Dad!" + textBox.getText().toString());
+
+                shownText.setText(shownText.getText().toString() + " " + textBox.getText().toString());
+            }
+        });
+        textBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    if(textBox.getText().toString().equals("TJ"))
+                    {
+                        shownText.setText("TJ Rocks");
+                        textBox.setText("");
+                        textBox.setHint("That's a good Name");
+                    }
+
+                }
             }
         });
     }
